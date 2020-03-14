bb4d163171f217200a73e969de770f725a7c0e9b
Bojo Zahariev <bozhidar_zahariev@hotmail.com>
2020-03-14 11:55:50 +0000

    hand over started

--- src/script.js
+++ src/script.js
@@ -18,20 +18,57 @@ const handOverCon = document.querySelector('#handOverCon');
 
 //New
 const dateHo = document.querySelector('#dateHo');
-const newFormHo = document.querySelector('#newFormHo');
+const inputOff = document.querySelector('#inputOff');
+//radio
+const radioEarly = document.querySelector('#radioEarly');
+const radioLate = document.querySelector('#radioLate');
+const radioFull = document.querySelector('#radioFull');
+
+const inputOn = document.querySelector('#inputOn');
+//radio pairs
+const inc1 = document.querySelector('#inc1');
+const inc2 = document.querySelector('#inc2');
 const textInc = document.querySelector('#textInc');
+
+const fire1 = document.querySelector('#fire1');
+const fire2 = document.querySelector('#fire2');
 const textFire = document.querySelector('#textFire');
+
+const keys1 = document.querySelector('#keys1');
+const keys2 = document.querySelector('#keys2');
 const textKeys = document.querySelector('#textKeys');
+
+const cams1 = document.querySelector('#cams1');
+const cams2 = document.querySelector('#cams2');
 const textCams = document.querySelector('#textCams');
+
+const act1 = document.querySelector('#act1');
+const act2 = document.querySelector('#act2');
 const textAct = document.querySelector('#textAct');
+
+const doors1 = document.querySelector('#doors1');
+const doors2 = document.querySelector('#doors2');
 const textDoors = document.querySelector('#textDoors');
-const textIntBars = document.querySelector('#textIntBars');
-const textExtBars = document.querySelector('#textExtBars');
+
+const inBarrs1 = document.querySelector('#inBarrs1');
+const inBarrs2 = document.querySelector('#inBarrs2');
+const textIntBarrs = document.querySelector('#textIntBarrs');
+
+const extBarrs1 = document.querySelector('#extBarrs1');
+const extBarrs2 = document.querySelector('#extBarrs2');
+const textExtBarrs = document.querySelector('#textExtBarrs');
+
+const city1 = document.querySelector('#city1');
+const city2 = document.querySelector('#city2');
 const textCity = document.querySelector('#textCity');
+
+const comms1 = document.querySelector('#comms1');
+const comms2 = document.querySelector('#comms2');
 const textCom = document.querySelector('#textCom');
 
-const submitBtn = document.querySelector('#formSubmit');
-const input = document.querySelector('#input1');
+const newFormHo = document.querySelector('#newFormHo');
+const hoSignature = document.querySelector('#hoSignature');
+const hoFormSubmit = document.querySelector('#hoFormSubmit');
 
 //Patrol
 const patrolCon = document.querySelector('#patrolCon');
@@ -60,8 +97,11 @@ const dateFormat = () => {
   let month = addZero(dateObj.getMonth() + 1);
   let day = addZero(dateObj.getDate());
   let year = dateObj.getFullYear();
+  let hours = addZero(dateObj.getHours());
+  let minutes = addZero(dateObj.getMinutes());
 
-  issueDate = day + '/' + month + '/' + year;
+  issueDate = day + '/' + month + '/' + year + ' ' + hours + ':' + minutes;
+
   return issueDate;
 };
 
@@ -73,7 +113,23 @@ const addZero = i => {
   return i;
 };
 
-dateHo.textContent = dateFormat();
+//return to the main screen
+backBtn.addEventListener('click', e => {
+  clearScreen();
+  initialDiv.style.display = 'block';
+});
+
+//clear all divs
+const clearScreen = () => {
+  let containers = document.getElementsByClassName('containers');
+  Array.from(containers).forEach(element => {
+    element.style.display = 'none';
+  });
+  handOverBtnDiv.style.display = 'none';
+};
+
+//HO
+dateHo.textContent = dateFormat().slice(0, 10);
 
 hoBtn.addEventListener('click', e => {
   clearScreen();
@@ -88,17 +144,24 @@ newBtn.addEventListener('click', e => {
   newFormHo.style.display = 'block';
 });
 
-backBtn.addEventListener('click', e => {
-  clearScreen();
-  initialDiv.style.display = 'block';
-});
+//Checks which shift radio is checked
+const checkedShift = () => {
+  if (radioEarly.checked) {
+    return radioEarly.value;
+  } else if (radioLate.checked) {
+    return radioLate.value;
+  } else if (radioFull.checked) {
+    return radioFull.value;
+  }
+};
 
-const clearScreen = () => {
-  let containers = document.getElementsByClassName('containers');
-  Array.from(containers).forEach(element => {
-    element.style.display = 'none';
-  });
-  handOverBtnDiv.style.display = 'none';
+//Checks which pairs radio is checked
+const checkedPairs = (radio1, radio2, text) => {
+  if (radio1.checked) {
+    return radio1.value;
+  } else if (radio2.checked) {
+    return text.value;
+  }
 };
 
 //Display textarea if different radio is pressed
@@ -127,13 +190,36 @@ displayTextArea(radiosAct, textAct);
 const radiosDoors = document.getElementsByClassName('radioDoors');
 displayTextArea(radiosDoors, textDoors);
 const radiosIntBars = document.getElementsByClassName('radioIntBars');
-displayTextArea(radiosIntBars, textIntBars);
+displayTextArea(radiosIntBars, textIntBarrs);
 const radiosExtBars = document.getElementsByClassName('radioExtBars');
-displayTextArea(radiosExtBars, textExtBars);
+displayTextArea(radiosExtBars, textExtBarrs);
 const radiosCity = document.getElementsByClassName('radioCity');
 displayTextArea(radiosCity, textCity);
 const radiosCom = document.getElementsByClassName('radioCom');
 displayTextArea(radiosCom, textCom);
+
+//Send Item to the server
+newFormHo.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (inputOff.value.length > 1 && inputOn.value.length > 1 && hoSignature.value.length > 0) {
+    ipcRenderer.send('addItem', {
+      id: Date.now(),
+      type: 'handOver',
+      date: getToday().slice(0, 10),
+      collOf: inputOff.value,
+      collOn: inputOn.value,
+      shift: checkedShift()
+    });
+
+    playSound('success');
+    backToInitial();
+    form.reset();
+  } else {
+    playSound('fail');
+    fail();
+  }
+});
 
 //PATROL
 patrolBtn.addEventListener('click', e => {
